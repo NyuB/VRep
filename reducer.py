70150bd1888e5c7b877c6da7da00f6ef68c31008
NyuB <37740771+NyuB@users.noreply.github.com>
2020-11-17 11:18:59 +0100

    Reducer version python 2

---
+++ reducer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys
+salesTotal = 0
+oldKey = None
+for line in sys.stdin:
+	data = line.strip().split("\t")
+	if len(data) != 2:
+		continue
+	thisKey, thisSale = data
+	if oldKey is not None and oldKey !=thisKey:
+		print "{0}\t{1}".format(oldKey,salesTotal)
+		salesTotal = 0
+	oldKey = thisKey
+	salesTotal += float(thisSale)
+	if oldKey is not None:
+		print oldKey, '\t', salesTotal
