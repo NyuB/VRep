07890cee59ab50b245f60026f73438f94cb7e241
NyuB <37740771+NyuB@users.noreply.github.com>
2020-11-18 08:57:49 +0100

    Update bashutils.sh

--- bashutils.sh
+++ bashutils.sh
@@ -1,3 +1,3 @@
 head -50 ../data/purchases.txt | mapper.py | sort | reducer.py
 
-hadoop jar /usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-2.6.0-mr1- cdh5.5.0.jar –mapper mapper.py –reducer reducer.py –file mapper.py –file reducer.py –input myinput –output joboutput
+hadoop jar /usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-2.6.0-mr1-cdh5.12.0.jar -mapper mapper.py -reducer reducer.py -file mapper.py -file reducer.py -input myinput -output joboutput
