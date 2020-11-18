a9239a66a7588b926fff70809d16d3f48078fc73
NyuB <37740771+NyuB@users.noreply.github.com>
2020-11-18 09:09:17 +0100

    reducer version somme

--- reducer.py
+++ reducer.py
@@ -7,7 +7,7 @@ for line in sys.stdin:
 	if len(data) != 2:
 		continue
 	thisKey, thisSale = data
-	if oldKey is not None and oldKey !=thisKey:
+	if oldKey is not None and oldKey !=thisKey: #On considere les inputs deja triees par clef(role du sorter/shuffler)
 		print "{0}\t{1}".format(oldKey,salesTotal)
 		salesTotal = 0
 	oldKey = thisKey
