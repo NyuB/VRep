c64e8e8614e137112e0bd83b17f228ccf4051826
NyuB <decaeste@etud.insa-toulouse.fr>
2021-01-26 15:15:31 +0100

    mapperforum

---
+++ mapper_forum.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import csv
+import sys
+
+reader = csv.reader(sys.stdin, delimiter='\t')
+writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QHOTE_ALL)
+
+P = ".!?"
+
+def countAll(msg,l):
+	res = 0
+	for c in l:
+		res+=msg.count(c)
+	return res
+
+def isPhrase(msg):
+	if len(msg) == 0:
+		return False
+	c = countAll(msg,P)
+	if c>1:
+		return False
+	elif c==0:
+		return True
+	elif not(msg[-1] in P):
+		return False
+
+for line in reader:
+	msg = line[4]
+	if isPhrase(msg):
+		writer.writerow(line)
